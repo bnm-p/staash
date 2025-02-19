af48e2104658dd18a40bd5077e863083c2c37ca7
Czepl <20190050@students.htl-perg.ac.at>
2025-02-19 21:55:45 +0100

    fix: variable context hono

--- apps/web/src/app/api/[...route]/route.ts
+++ apps/web/src/app/api/[...route]/route.ts
@@ -6,41 +6,43 @@ import { auth } from "@/lib/auth";
 
 export const runtime = "edge";
 
-const app = new Hono<{
-	Variables: {
-		user: typeof auth.$Infer.Session.user | null;
-		session: typeof auth.$Infer.Session.session | null;
-	};
-}>().basePath("/api");
+declare module "hono" {
+  interface ContextVariableMap {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null;
+  }
+}
+
+const app = new Hono().basePath("/api");
 
 app.use("*", async (c, next) => {
-	const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
 
-	if (!session) {
-		c.set("user", null);
-		c.set("session", null);
-		return next();
-	}
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
 
-	c.set("user", session.user);
-	c.set("session", session.session);
-	return next();
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
 });
 
 app.use(
-	"/auth/*", // or replace with "*" to enable cors for all routes
-	cors({
-		origin: "http://localhost:3000", // replace with your origin
-		allowHeaders: ["Content-Type", "Authorization"],
-		allowMethods: ["POST", "GET", "OPTIONS"],
-		exposeHeaders: ["Content-Length"],
-		maxAge: 600,
-		credentials: true,
-	}),
+  "/auth/*", // or replace with "*" to enable cors for all routes
+  cors({
+    origin: "http://localhost:3000", // replace with your origin
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  })
 );
 
 app.on(["POST", "GET"], "/auth/*", (c) => {
-	return auth.handler(c.req.raw);
+  return auth.handler(c.req.raw);
 });
 
 const routes = app.route("/hello", helloRouter);
--- apps/web/src/app/api/[...route]/routers/hello-router.ts
+++ apps/web/src/app/api/[...route]/routers/hello-router.ts
@@ -1,8 +1,13 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 export const helloRouter = new Hono().get("/", async (c) => {
-	const user = c.get("user");
-	const message = user ? `Hello ${user.name}!` : "Hello from Hono!";
+  const user = c.get("user");
 
-	return c.json({ message: message });
+  if (!user) {
+    throw new HTTPException(401, {message: 'Unauthorized'})
+  }
+  const message = user ? `Hello ${user.name}!` : "Hello from Hono!";
+
+  return c.json({ message: message });
 });
