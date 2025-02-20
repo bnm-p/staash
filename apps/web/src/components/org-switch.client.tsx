391e3a2848ba094354e9ab37f28c09a2e02f7401
bnm <bnm.wag@gmail.com>
2025-02-20 08:53:17 +0100

    feat(org-switch): handle organization and space handling in own dialog

--- apps/web/src/components/org-switch.client.tsx
+++ apps/web/src/components/org-switch.client.tsx
@@ -2,20 +2,29 @@
 
 import { authClient } from "@/lib/auth-client";
 import type { Organization } from "@prisma/client";
-import { useQuery } from "@tanstack/react-query";
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+} from "@workspace/ui/components/avatar";
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from "@workspace/ui/components/command";
 import {
 	Popover,
 	PopoverContent,
 	PopoverTrigger,
 } from "@workspace/ui/components/popover";
-import { Skeleton } from "@workspace/ui/components/skeleton";
 import { cn } from "@workspace/ui/lib/utils";
-import { useCallback, useEffect, useState, type FC } from "react";
-import Image from "next/image";
-import { ChevronsUpDown, Plus, PlusCircle, Rotate3D } from "lucide-react";
+import { Check, ChevronsUpDown, Plus } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
+import { type FC, useCallback, useEffect, useRef, useState } from "react";
 import type { IOrgSwitchProps } from "./org-switch";
-import { Button } from "@workspace/ui/components/button";
 
 interface IOrgSwitchClientProps extends IOrgSwitchProps {
 	organizations: Organization[];
@@ -32,14 +41,36 @@ export const OrgSwitchClient: FC<IOrgSwitchClientProps> = ({
 	const { data: activeOrganization, isPending } =
 		authClient.useActiveOrganization();
 
+	const orgsCommandRef = useRef<HTMLInputElement>(null);
+	const spacesCommandRef = useRef<HTMLInputElement>(null);
+
+	const handleKeyDown = useCallback(
+		(e: KeyboardEvent) => {
+			if (!open) return;
+			if (e.key === "ArrowRight") {
+				spacesCommandRef.current?.focus();
+			} else if (e.key === "ArrowLeft") {
+				orgsCommandRef.current?.focus();
+			}
+		},
+		[open],
+	);
+
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyDown);
+		return () => document.removeEventListener("keydown", handleKeyDown);
+	}, [handleKeyDown]);
+
 	const handleOrgChange = useCallback(
 		async (org: Organization) => {
 			if (!org.slug) return;
 
-			await authClient.organization.setActive({
+			void authClient.organization.setActive({
 				organizationSlug: org.slug,
 			});
 
+			setOpen(false);
+			router.refresh();
 			router.push(`/${org.slug}`);
 		},
 		[router],
@@ -58,96 +89,143 @@ export const OrgSwitchClient: FC<IOrgSwitchClientProps> = ({
 	}, [pathname, handleOrgChange, organizations, router]);
 
 	const handleCreateOrg = () => {
-		router.push("/create");
+		router.push("/create/org");
+	};
+
+	const handleCreateSpace = () => {
+		router.push("/create/space");
 	};
 
 	return (
 		<Popover open={open} onOpenChange={setOpen}>
 			<PopoverTrigger asChild>
-				<Button
-					variant={"ghost"}
-					type="button"
-					className={cn("flex items-center justify-between w-44", className)}
-					{...props}
-				>
-					<span className="text-sm font-medium flex items-center gap-2">
-						{isPending ? (
-							<>
-								<Skeleton className="h-4 w-4" />
-								<Skeleton className="h-4 w-12" />
-							</>
-						) : (
-							<>
-								<div className="relative h-4 w-4 overflow-hidden bg-muted flex items-center justify-center">
-									{activeOrganization?.logo ? (
-										<Image
-											src={activeOrganization?.logo}
-											alt={`${activeOrganization?.name}'s logo`}
-											fill
-											className="object-cover"
-										/>
-									) : (
-										<span className="text-xs text-muted-foreground scale-75">
-											{activeOrganization?.name?.[0]}
-										</span>
-									)}
-								</div>
-								{activeOrganization?.name}
-							</>
-						)}
-					</span>
-					<ChevronsUpDown className="size-3 text-muted-foreground" />
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-[200px] p-0" align="start" sideOffset={10}>
-				{organizations?.map((org) => (
-					<button
-						key={org.id}
-						type="button"
-						className={cn(
-							"flex items-center gap-2 w-full px-4 py-2 text-sm transition-colors",
-							org.id === activeOrganization?.id
-								? "text-muted-foreground opacity-50"
-								: "bg-background text-white hover:bg-muted",
-						)}
-						onClick={() => {
-							if (org.id !== activeOrganization?.id) {
-								handleOrgChange(org as Organization);
-								setOpen(false);
-							}
-						}}
-					>
-						<div className="relative h-4 w-4 overflow-hidden bg-muted flex items-center justify-center">
-							{org.logo ? (
-								<Image
-									src={org.logo}
-									alt={`${org.name}'s logo`}
-									fill
-									className="object-cover"
-								/>
-							) : (
-								<span className="text-xs text-muted-foreground scale-75">
-									{org.name?.[0]}
-								</span>
-							)}
-						</div>
-						<span>{org.name}</span>
-					</button>
-				))}
 				<button
 					type="button"
-					className={cn(
-						"flex items-center gap-2 w-full px-4 py-2 text-sm transition-colors border-t border-border",
-						"bg-background text-white hover:bg-muted",
-					)}
-					onClick={() => {
-						handleCreateOrg();
-						setOpen(false);
-					}}
+					aria-expanded={open}
+					aria-label="Select organization and space"
+					className="h-full w-[256px] justify-between flex items-center px-8 hover:bg-muted"
+					{...props}
 				>
-					<PlusCircle className="text-primary size-4" />
-					<span>Create Organization</span>
+					<div className="flex items-center gap-2">
+						<Avatar className="size-8">
+							<AvatarImage
+								src={activeOrganization?.logo || ""}
+								alt={activeOrganization?.name}
+							/>
+							<AvatarFallback>{activeOrganization?.name[0]}</AvatarFallback>
+						</Avatar>
+						<span className="text-sm">
+							{activeOrganization?.name}
+							{/* {selectedSpace ? ` / ${selectedSpace.name}` : ""} */}
+						</span>
+					</div>
+					<ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
 				</button>
+			</PopoverTrigger>
+			<PopoverContent
+				className="w-[516px] p-0 border-none"
+				align="start"
+				alignOffset={-1}
+				sideOffset={0}
+			>
+				<div className="grid grid-cols-2 overflow-hidden border bg-background shadow-sm">
+					<div className="border-r">
+						<Command className="h-full border-none">
+							<CommandInput
+								ref={orgsCommandRef}
+								placeholder="Find Organization..."
+							/>
+							<CommandList>
+								<CommandEmpty>No organizations found.</CommandEmpty>
+								<CommandGroup heading="Organizations" className="p-0">
+									{organizations.map((org) => (
+										<CommandItem
+											key={org.id}
+											onSelect={() => handleOrgChange(org)}
+											className="flex items-center gap-2 px-4 py-2 data-[selected=true]:bg-muted"
+										>
+											<Avatar className="h-8 w-8">
+												<AvatarImage src={org.logo || ""} alt={org.name} />
+												<AvatarFallback>{org.name[0]}</AvatarFallback>
+											</Avatar>
+											<span>{org.name}</span>
+											<Check
+												className={cn(
+													"ml-auto h-4 w-4",
+													activeOrganization?.id === org.id
+														? "opacity-100"
+														: "opacity-0",
+												)}
+											/>
+										</CommandItem>
+									))}
+									<CommandItem
+										onSelect={handleCreateOrg}
+										className="flex items-center gap-2 px-4 py-2 text-sm data-[selected=true]:bg-muted"
+									>
+										<div className="flex h-8 w-8 items-center justify-center rounded-full border border-dashed">
+											<Plus className="h-4 w-4" />
+										</div>
+										Create Team
+									</CommandItem>
+								</CommandGroup>
+							</CommandList>
+						</Command>
+					</div>
+					<div>
+						<Command className="h-full border-none">
+							<CommandInput
+								ref={spacesCommandRef}
+								placeholder="Find Space..."
+							/>
+							<CommandList>
+								<CommandEmpty>No spaces found.</CommandEmpty>
+								<CommandGroup heading="Spaces" className="p-0">
+									{/* {spaces.map((space) => (
+										<CommandItem
+											key={space.id}
+											onSelect={() => {
+												setSelectedSpace(space);
+												setOpen(false);
+											}}
+											className="flex items-center gap-2 px-4 py-2 data-[selected=true]:bg-muted"
+										>
+											<div className="flex h-6 w-6 items-center justify-center border bg-muted">
+												{space.icon ? (
+													<img
+														src={space.icon || "/placeholder.svg"}
+														alt=""
+														className="h-4 w-4"
+													/>
+												) : (
+													<div className="h-4 w-4" />
+												)}
+											</div>
+											<span>{space.name}</span>
+											<Check
+												className={cn(
+													"ml-auto h-4 w-4",
+													selectedSpace?.id === space.id
+														? "opacity-100"
+														: "opacity-0",
+												)}
+											/>
+										</CommandItem>
+									))} */}
+									<CommandItem
+										onSelect={handleCreateSpace}
+										className="flex items-center gap-2 px-4 py-2 text-sm data-[selected=true]:bg-muted"
+									>
+										<div className="flex h-6 w-6 items-center justify-center border border-dashed">
+											<Plus className="h-4 w-4" />
+										</div>
+										Create Space
+									</CommandItem>
+								</CommandGroup>
+							</CommandList>
+						</Command>
+					</div>
+				</div>
 			</PopoverContent>
 		</Popover>
 	);
