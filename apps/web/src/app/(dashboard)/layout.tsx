938d3d5852d17b5ac1cc42ad29bb23f0cf53d57d
bnm <bnm.wag@gmail.com>
2025-02-20 08:52:04 +0100

    chore(nav): separate nav from layout into own comp

--- apps/web/src/app/(dashboard)/layout.tsx
+++ apps/web/src/app/(dashboard)/layout.tsx
@@ -1,9 +1,7 @@
+import { Nav } from "@/components/nav";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { Logo } from "@/components/logo";
-import { OrgSwitch } from "@/components/org-switch";
-import { UserProfile } from "@/components/user-profile";
 
 export default async function DashboardLayout({
 	children,
@@ -20,16 +18,7 @@ export default async function DashboardLayout({
 		<div>
 			<div className="flex border-b h-14 border-border">
 				<div className="w-72 border-r border-border" />
-				<div className="flex-grow flex items-center justify-between px-4">
-					<div className="flex items-center gap-8">
-						<Logo />
-						<div className="h-6 w-px bg-border rotate-6" />
-						<OrgSwitch />
-					</div>
-					<div>
-						<UserProfile />
-					</div>
-				</div>
+				<Nav />
 				<div className="w-72 border-l border-border" />
 			</div>
 			<div className="flex border-b min-h-[calc(100svh-3.5rem)] border-border">
