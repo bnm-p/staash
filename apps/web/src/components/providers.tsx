62c6c6793f4a84d05c1adb6a642b69691bce91e7
bnm <bnm.wag@gmail.com>
2025-02-20 17:49:32 +0100

    feat: add toaster component to actually render toasts

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import type * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type * as React from "react";
+import { Toaster } from "sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	const queryClient = new QueryClient();
 
 	return (
 		<NextThemesProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange enableColorScheme>
+			<Toaster theme="dark" toastOptions={{ className: "rounded-none" }} />
 			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 		</NextThemesProvider>
 	);
