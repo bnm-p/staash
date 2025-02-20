34862b21c64a2deecb12f0c2c295c4a9c3bcb10e
Czepl <20190050@students.htl-perg.ac.at>
2025-02-20 17:29:19 +0100

    refactor: update routes and forms, remove unused router

--- apps/web/src/components/forms/create-space-form.tsx
+++ apps/web/src/components/forms/create-space-form.tsx
@@ -5,15 +5,7 @@ import { client } from "@/lib/client";
 import { slugify } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@workspace/ui/components/button";
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "@workspace/ui/components/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@workspace/ui/components/form";
 import { Input } from "@workspace/ui/components/input";
 import { cn } from "@workspace/ui/lib/utils";
 import { useParams, useRouter } from "next/navigation";
@@ -25,124 +17,109 @@ import * as z from "zod";
 interface ICreateOrgFormProps extends React.ComponentProps<"div"> {}
 
 const formSchema = z.object({
-	name: z.string(),
-	slug: z.string(),
+  name: z.string(),
+  slug: z.string(),
 });
 
-export const CreateSpaceForm: FC<ICreateOrgFormProps> = ({
-	className,
-	...props
-}) => {
-	const router = useRouter();
-	const [autoSlug, setAutoSlug] = useState(true);
-	const { data: activeOrganization } = authClient.useActiveOrganization();
+export const CreateSpaceForm: FC<ICreateOrgFormProps> = ({ className, ...props }) => {
+  const router = useRouter();
+  const [autoSlug, setAutoSlug] = useState(true);
+  const { data: activeOrganization } = authClient.useActiveOrganization();
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			name: "",
-			slug: "",
-		},
-	});
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      slug: "",
+    },
+  });
 
-	const watchName = form.watch("name", "");
+  const watchName = form.watch("name", "");
 
-	useEffect(() => {
-		if (autoSlug && watchName) {
-			form.setValue("slug", slugify(watchName));
-		}
-	}, [watchName, autoSlug, form]);
+  useEffect(() => {
+    if (autoSlug && watchName) {
+      form.setValue("slug", slugify(watchName));
+    }
+  }, [watchName, autoSlug, form]);
 
-	async function onSubmit(values: z.infer<typeof formSchema>) {
-		try {
-			if (!activeOrganization) {
-				throw new Error("No active organization");
-			}
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      if (!activeOrganization) {
+        throw new Error("No active organization");
+      }
 
-			const res = await client.api.create.space.$post({
-				form: {
-					...values,
-					orgId: activeOrganization.id,
-				},
-			});
+      const res = await client.api.org.space.$post({
+        form: {
+          ...values,
+          orgId: activeOrganization.id,
+        },
+      });
 
-			const data = await res.json();
+      const data = await res.json();
 
-			toast.success("Space created");
-			router.refresh();
-			router.push(`/${activeOrganization.slug}/${data.slug}`);
-		} catch (error) {
-			console.error("Form submission error", error);
-			toast.error("Failed to submit the form. Please try again.");
-		}
-	}
+      toast.success("Space created");
+      router.refresh();
+      router.push(`/${activeOrganization.slug}/${data.slug}`);
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
 
-	return (
-		<div className={cn("", className)} {...props}>
-			<div>
-				<h1 className="text-2xl font-semibold">Create Space</h1>
-				<p className="text-muted-foreground">
-					Set up your new space in seconds
-				</p>
-			</div>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className={cn("space-y-8 w-full py-10", className)}
-				>
-					<FormField
-						control={form.control}
-						name="name"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Name</FormLabel>
-								<FormControl>
-									<Input
-										type=""
-										className="max-w-md"
-										placeholder="My Space"
-										{...field}
-									/>
-								</FormControl>
-								<FormDescription>Name of your space</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+  return (
+    <div className={cn("", className)} {...props}>
+      <div>
+        <h1 className="text-2xl font-semibold">Create Space</h1>
+        <p className="text-muted-foreground">Set up your new space in seconds</p>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className={cn("space-y-8 w-full py-10", className)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="" className="max-w-md" placeholder="My Space" {...field} />
+                </FormControl>
+                <FormDescription>Name of your space</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-					<FormField
-						control={form.control}
-						name="slug"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Slug</FormLabel>
-								<FormControl className="flex items-stretch">
-									<div className="max-w-md">
-										<div className="px-4 py-2 bg-muted border border-r-0 border-border text-muted-foreground text-sm flex items-center">
-											staash.app/{activeOrganization?.slug}/
-										</div>
-										<Input
-											type=""
-											placeholder="my-space"
-											{...field}
-											onChange={(e) => {
-												setAutoSlug(false);
-												field.onChange(e);
-											}}
-										/>
-									</div>
-								</FormControl>
-								<FormDescription>URL slug of your space</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
+          <FormField
+            control={form.control}
+            name="slug"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Slug</FormLabel>
+                <FormControl className="flex items-stretch">
+                  <div className="max-w-md">
+                    <div className="px-4 py-2 bg-muted border border-r-0 border-border text-muted-foreground text-sm flex items-center">staash.app/{activeOrganization?.slug}/</div>
+                    <Input
+                      type=""
+                      placeholder="my-space"
+                      {...field}
+                      onChange={(e) => {
+                        setAutoSlug(false);
+                        field.onChange(e);
+                      }}
+                    />
+                  </div>
+                </FormControl>
+                <FormDescription>URL slug of your space</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-					<Button type="submit" className="w-full max-w-md">
-						Create Space
-					</Button>
-				</form>
-			</Form>
-		</div>
-	);
+          <Button type="submit" className="w-full max-w-md">
+            Create Space
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
