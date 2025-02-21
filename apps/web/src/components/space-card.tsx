d84c136754cefefb5793c0789d751586c0ccc8f7
bnm <bnm.wag@gmail.com>
2025-02-21 19:09:58 +0100

    fix: spread props on top level

--- apps/web/src/components/space-card.tsx
+++ apps/web/src/components/space-card.tsx
@@ -7,7 +7,7 @@ import { Container, Shield, Variable } from "lucide-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
-export interface ISpaceCardProps extends React.ComponentProps<"div"> {
+export interface ISpaceCardProps extends React.ComponentProps<"a"> {
 	space: Space;
 	index: number;
 }
@@ -19,12 +19,13 @@ export const SpaceCard: FC<ISpaceCardProps> = ({ className, space, index, ...pro
 		<Link
 			href={`/orgs/${params.orgSlug}/spaces/${space.slug}`}
 			className={cn("border-border border-b", index % 2 === 0 ? "border-r" : "", className)}
+			{...props}
 		>
 			<div className="flex items-end gap-4 px-8 py-6">
 				<div className="h-12 w-12 border border-border bg-muted" />
 				<p className="text-xl">{space.name}</p>
 			</div>
-			<div className="grid grid-cols-3 gap-4 border-muted border-t px-8 py-6 pt-6 ">
+			<div className="grid grid-cols-3 gap-4 border-muted border-t px-8 py-6 pt-6">
 				<div className="space-y-2">
 					<div className="text-2xl">12</div>
 					<div className="flex items-center gap-x-2 text-muted-foreground text-xs">
