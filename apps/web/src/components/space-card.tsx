bd6e794c48f960492e9a412d8d524f6be6d36eee
bnm <bnm.wag@gmail.com>
2025-02-21 19:03:05 +0100

    refactor: feedback

--- apps/web/src/components/space-card.tsx
+++ apps/web/src/components/space-card.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { cn } from "@workspace/ui/lib/utils";
 import type { Space } from "@prisma/client";
 import type { FC } from "react";
 import { Container, Shield, Variable } from "lucide-react";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export interface ISpaceCardProps extends React.ComponentProps<"div"> {
 	space: Space;
@@ -9,35 +13,40 @@ export interface ISpaceCardProps extends React.ComponentProps<"div"> {
 }
 
 export const SpaceCard: FC<ISpaceCardProps> = ({ className, space, index, ...props }) => {
+	const params = useParams();
+
 	return (
-		<div className={cn("border-border border-b p-6", index % 2 === 0 ? "border-r" : "", className)}>
-			<div className="flex">
-				<div className="h-12 w-12 bg-red-500" />
-				<p className="flex items-end pl-6 text-xl">{space.name}</p>
+		<Link
+			href={`/orgs/${params.orgSlug}/spaces/${space.slug}`}
+			className={cn("border-border border-b", index % 2 === 0 ? "border-r" : "", className)}
+		>
+			<div className="flex items-end gap-4 px-8 py-6">
+				<div className="h-12 w-12 border border-border bg-muted" />
+				<p className="text-xl">{space.name}</p>
 			</div>
-			<div className="grid grid-cols-3 gap-4 pt-4">
-				<div>
+			<div className="grid grid-cols-3 gap-4 border-muted border-t px-8 py-6 pt-6 ">
+				<div className="space-y-2">
 					<div className="text-2xl">12</div>
-					<div className="flex pt-4">
-						<Container color="gray" />
-						<div className="pl-2 text-gray-400">Members</div>
+					<div className="flex items-center gap-x-2 text-muted-foreground text-xs">
+						<Container className="size-4" />
+						<p>Environments</p>
 					</div>
 				</div>
-				<div>
+				<div className="space-y-2">
 					<div className="text-2xl">12</div>
-					<div className="flex pt-4">
-						<Shield color="gray" />
-						<div className="pl-2 text-gray-400">Members</div>
+					<div className="flex items-center gap-x-2 text-muted-foreground text-xs">
+						<Shield className="size-4" />
+						<p>Secrets</p>
 					</div>
 				</div>
-				<div>
+				<div className="space-y-2">
 					<div className="text-2xl">12</div>
-					<div className="flex pt-4">
-						<Variable color="gray" />
-						<div className="pl-2 text-gray-400">Members</div>
+					<div className="flex items-center gap-x-2 text-muted-foreground text-xs">
+						<Variable className="size-4" />
+						<p>Variables</p>
 					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
