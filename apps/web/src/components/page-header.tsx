d89b35690359f85db301809a7eee6dd4ae9c9e73
bnm <bnm.wag@gmail.com>
2025-02-23 15:03:58 +0100

    feat: make page header sticky on top and shrink in size

--- apps/web/src/components/page-header.tsx
+++ apps/web/src/components/page-header.tsx
@@ -1,16 +1,30 @@
+"use client";
+
+import { useEffect, type FC } from "react";
 import { cn } from "@workspace/ui/lib/utils";
-import type { FC } from "react";
 import type React from "react";
+import useScrollPosition from "@react-hook/window-scroll";
+import { useRange } from "@/hooks/use-range";
 
 interface IPageHeaderProps extends React.ComponentProps<"div"> {
 	title: string;
 }
 
 export const PageHeader: FC<IPageHeaderProps> = ({ title, className, ...props }) => {
+	const y = useScrollPosition(60);
+	const navHeight = useRange(y, 0, 50, 112, 64);
+	const textScale = useRange(y, 0, 50, 1, 0.5);
+
 	return (
-		<div className={cn("flex h-32 w-full items-center border-border border-b", className)} {...props}>
-			<div className="container mx-auto">
-				<h1>{title}</h1>
+		<div
+			className={cn("sticky top-0 flex h-16 w-full items-center border-border border-b", className)}
+			style={{ height: `${navHeight}px` }}
+			{...props}
+		>
+			<div className="container">
+				<h1 className="origin-[center_left]" style={{ transform: `scale(${textScale})` }}>
+					{title}
+				</h1>
 			</div>
 		</div>
 	);
