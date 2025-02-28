8dc02ed4aa589097e06bd2445ebf9b903eb505c3
bnm <bnm.wag@gmail.com>
2025-02-28 23:05:06 +0100

    fix: recursive redirects

--- apps/web/src/app/(dashboard)/page.tsx
+++ apps/web/src/app/(dashboard)/page.tsx
@@ -6,8 +6,12 @@ import type { NextPage } from "next";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-const IndexPage: NextPage = async () => {
-	//TODO entweder orgsService Fetch nehmen oder ois auslogern --> HF bingo
+interface IIndexPageProps {
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+const IndexPage: NextPage<IIndexPageProps> = async ({ searchParams }) => {
+	const params = await searchParams;
 	const session = await auth.api.getSession({ headers: await headers() });
 
 	if (!session?.user) {
@@ -25,6 +29,10 @@ const IndexPage: NextPage = async () => {
 	}
 
 	const organizations = await usersService.getAllOrgsForCurrentUser(session?.user.id);
+
+	if (params.modalOpen === "create-org") {
+		return;
+	}
 
 	if (organizations?.length === 0) {
 		return redirect("/?modalOpen=create-org");
